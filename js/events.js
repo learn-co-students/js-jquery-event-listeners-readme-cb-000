1363fba11019251a89d30d0373f1e070ca03cf88
Mark Fowler <fowlermark488@gmail.com>
2017-01-26 22:59:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
 //define functions here
+getIt = () => {
+  $('p').on('click', () => {
+    alert("Hey!");
+});
+}
+
+frameIt = () => {
+  $('img').on('load', () => {
+    $('img').addClass('tasty');
+});
+}
+
+pressIt = () => {
+  $('form').on('keydown', (key) => {
+    if (key.which == 71 ) {
+      alert('g was pressed');
+    }
+  });
+}
+
+submitIt = () => {
+  $('form').on('submit', () => {
+    alert('Your form is going to be submitted now.');
+  });
+
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
