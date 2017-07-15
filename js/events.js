99152ee317d6defe2ab25e1cf1d495ac0d6a59d7
Alexander Thibodeaux <alyxanderthibodeaux@gmail.com>
2017-07-15 20:33:43 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(e) {
+    $(this).addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('form input#typing').on('keydown', function(e) {
+    if(e.which == 71) {
+      alert('g was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
