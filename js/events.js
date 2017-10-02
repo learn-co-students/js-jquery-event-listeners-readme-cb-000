d6959ce83005ec7212cbcc1e395dec1975f8aac0
Jordan Johnson <jkj86@hotmail.com>
2017-10-02 12:07:30 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
-//define functions here
+function getIt(){
+  alert("Hey!");
+}
+
+function frameIt(){
+  document.querySelector("img").className = "tasty";
+}
+
+function submitIt(){
+  alert('Your form is going to be submitted now.');
+  return;
+}
+
+function pressIt(key){
+  if(key == 71){
+      alert('G was pressed.');
+  }
+  else{
+    //alert('G was not pressed.');
+  }
+  return;
+}
 
 $(document).ready(function(){
 
-// call functions here
+  $(p).on('click', function(key) {
+    getIt();
+  });
+
+  $("form").on("submit", function() {
+    submitIt();
+  });
+
+  $("#typing").on('keydown', function(key) {
+    pressIt(key.keyCode);
+  });
 
 });
