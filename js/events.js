5c706cc02c24c7e7acf73c3736a5431bc039364e
Matt Hough <matt@hough.tv>
2018-01-03 08:55:14 +1000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,30 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $("img:first").addClass("tasty");
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert("Your form is going to be submitted now.")
+    return false;
+  });
+}
+
+function pressIt() {
+  $("input").on("keydown", function(key) {
+    if (parseInt(key.which) === 71) {
+      alert("You just pressed G.");
+    }
+  });
+}
 
 $(document).ready(function(){
 
