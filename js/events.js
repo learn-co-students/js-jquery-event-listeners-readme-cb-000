da7785f1fd48198c9e5fc9bc76e4c9cc69304eca
Michael Wahlin <wahlin@gmail.com>
2018-02-06 12:54:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
-
 });
+
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!");
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+function pressIt() {
+  $('form').on("keydown", function(key){
+    if (key.which === 71) {
+      alert('G was pressed')
+    }
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass("tasty")
+  })
+}
