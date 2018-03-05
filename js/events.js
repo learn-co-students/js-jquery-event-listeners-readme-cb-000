a1f72e2c44eca4636daa0a60c91c1eb51de0a9d6
Alex Segers <a.segers.dev@gmail.com>
2018-03-05 23:32:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -6,22 +6,30 @@ function getIt() {
 
 function frameIt() {
   $('img').on('load', function() {
-    $('img').addClass('tasty');
-  });
-}
-
-function pressIt() {
-  $('#typing').keydown(function(e) {
-    if (e.which == 70) alert('You\'ve selected the letter \'G\'');
+    $('img').addClass("tasty");
   });
 }
 
 function submitIt() {
-  $('form').on('submit', function {
+  $("form").on("submit", function() {
     alert('Your form is going to be submitted now.');
+    return;
   });
-}
+};
 
-$(document).ready(function(){
 
+function pressIt() {
+  $('#typing').on('keydown', function(k) {
+    if (k.which == 71) {
+      alert("'G' was pressed!");
+    };
+  });
+};
+
+
+$(document).ready(function() {
+  getIt();
+  frameIt();
+  submitIt();
+  pressIt();
 });
