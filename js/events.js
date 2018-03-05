d83b7442a76ec27a686457753a6ac0ab1b7629ec
Alex Segers <a.segers.dev@gmail.com>
2018-03-05 20:25:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
-//define functions here
+function getIt() {
+  alert("Hey!");
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+}
+
+function pressIt(e) {
+  if (e.which == 70) {alert('You\'ve selected the letter \'G\'')}
+}
+
+function submitIt() {
+  alert('Your form is going to be submitted now.');
+}
 
 $(document).ready(function(){
 
-// call functions here
+  $('p').on('click', getIt());
+
+  $('img').on('load', frameIt());
+
+  $('#typing').keydown(pressIt(e));
+
+  $([type='submit']).on('submit', submitIt());
 
 });
