367e903909bbc7d1f8b994ad63b51b7f99969544
youssefmys <mosa500@gmail.com>
2018-03-08 12:34:20 +0200

    Done

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function(event) {
+    alert("Hey!")
+  });
+}
 
-// call functions here
+function frameIt() {
+  image = $('img')
+  image.on('load', function(event) {
+    image.addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('form input[type=text]').on('keydown', function(event) {
+    if(event.which == 71) {
+      alert("You've pressed letter G")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(event) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+
+$(document).ready(function(){
+  getIt() ;
+  pressIt() ;
+  submitIt() ; 
+  frameIt() ;
 
 });
