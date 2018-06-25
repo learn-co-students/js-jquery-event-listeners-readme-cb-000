23ae1cee8c4f43365f2a32dbe0f9a8248512a242
Tao <werewaswas@live.com>
2018-06-25 04:09:26 +0000

    t

--- js/events.js
+++ js/events.js
@@ -10,4 +10,22 @@ function getIt () {
   $('p').on('click', function () {
       alert('Hey')
     })
-}  
+}
+
+function frameIt () {
+  $('img').on('load', function () {
+      $('img').addClass('tasty');
+    })
+}
+
+function submitIt () {
+  $('form').on('submit', function () {
+      alert('Your form is going to be submitted now.')
+    })
+}
+
+function pressIt () {
+  $('form').on('submit', function () {
+      alert('Your form is going to be submitted now.')
+    })
+}
