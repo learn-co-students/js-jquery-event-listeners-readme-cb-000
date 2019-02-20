40ca844337ea685d7397693a718da39fc4f570dc
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-20 20:27:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,30 @@
 
 $(document).ready(function(){
 
-// call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
+
+function getIt() {
+  $('p').on("click", function() {
+    alert("Hey!") ;
+  })
+}
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty") ;
+  })
+}
+function pressIt() {
+  $('#typing').on("keydown", function(e) {
+    if (e.which === 71) {alert('You have pressed the G key')}
+  })
+}
+function submitIt() {
+  $('form').on("submit", function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
